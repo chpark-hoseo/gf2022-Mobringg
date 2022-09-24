3ee2f548dbda6646ec484607006fbd0465e5d419
Mobringg <com2se20@gmail.com>
2022-09-24 12:12:54 +0900

    SDL 그리기 실습
    실습시간이 커밋이 안되어서 다시 합니다

---
+++ src/Game.h
@@ -0,0 +1,27 @@
+#include <SDL2/SDL.h>
+
+class Game
+{
+public:
+	Game() { }
+	~Game() { }
+
+	bool init(const char* title, int xpos, int ypos, int height, int width, int flags);
+	void render();
+	void update();
+	bool running();
+	void handleEvents();
+	void clean();
+
+private:
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+	SDL_Texture* m_pTexture;
+
+	// 원본 사각형
+	SDL_Rect m_sourceRectangle;
+
+	// 대상 사각형
+	SDL_Rect m_destinationRectangle;
+};
