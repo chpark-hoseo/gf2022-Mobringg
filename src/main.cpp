75d14391bbfc7a81049ae5e633b698d3a4adf071
Mobringg <com2se20@gmail.com>
2022-09-24 12:04:31 +0900

    game 클래스화 main 수정
    수정

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,20 @@
-﻿#include <SDL2/SDL.h>
-#include <cstdlib>
-#include <ctime>
-#include <stdio.h>
-#include <iostream>
-using namespace std;
-
-
-
-#include "main.h"
-
-
-	
-
-
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false;
-bool init(const char* title, int xpos, int ypos, int height, int width, int flags);
-void Render();
+﻿#include "Game.h"
+Game* g_game = 0;
 
 int main(int argc, char* argv[])
 {
-	if (init("Breaking Up HelloSDL", SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED, 640, 480,
-		SDL_WINDOW_SHOWN))
-	{
-		g_bRunning = true;
-	}
-	else
-	{
-		return 1;
-	}
-	while (g_bRunning < 10)
-	{
-		g_bRunning++;
-		SDL_SetRenderDrawColor(g_pRenderer, rand() % 256, rand() % 256, rand() % 256, 255);
-		SDL_Delay(1000);
-		Render();
+    g_game = new Game();
 
-		SDL_Event event;
-		if (SDL_PollEvent(&event)) {
-			switch (event.type) {
-			case SDL_KEYDOWN:
-				printf("Key press detected\n");
-				SDL_Quit();
-				break;
-			case SDL_KEYUP:
-				printf("Key release detected\n");
-				break;
-			}
-		}
-	}
+    g_game->init("Game Class", 100, 100, 640, 480, 0);
 
-	SDL_Quit();
-	return 0;
+    while (g_game->running())
+    {
+        g_game->handleEvents();
+        g_game->update();
+        g_game->render();
+    }
+
+    g_game->clean();
+
+    return 0;
 }
-
-bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
-		g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
-
-		if (g_pWindow != 0) {
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-	}
-	else {
-		return false;
-	}
-
-	return true;
-}
-
-void Render()
-{
-	SDL_RenderClear(g_pRenderer);
-
-	SDL_RenderPresent(g_pRenderer);
-}
-
