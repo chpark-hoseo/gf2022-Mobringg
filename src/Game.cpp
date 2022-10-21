a4c6a2b42fedfe1786ea637fb45ce345f4a4213a
Mobringg <com2se20@gmail.com>
2022-10-21 09:02:25 +0900

    임시2
    123

--- src/Game.cpp
+++ src/Game.cpp
@@ -11,6 +11,7 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
             if (m_pRenderer != 0) {
                 SDL_SetRenderDrawColor(
                     m_pRenderer, 255, 255, 255, 255);
+                
             }
             else {
                 return false; // 랜더러 생성 실패
@@ -25,7 +26,7 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
     };
 
  
-    if (!TheTextureManager::Instance()->load("assets/animate-alpha.png", "animate", m_pRenderer))
+    if (!TheTextureManager::Instance()->load("assets/145.bmp", "Chr", m_pRenderer))
     {
         return false;
     }
@@ -33,10 +34,47 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
     m_bRunning = true;
     return true;
 }
-
 void Game::update()
 {
-    m_currentFrame = ((SDL_GetTicks() / 100) % 6);
+    man();
+    m_currentFrame = ((SDL_GetTicks() / 100) % 5);
+    SDL_Delay(5);
+    
+}
+
+void Game::man()
+{
+    const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+    
+    if (currentKeyStates[SDL_SCANCODE_LEFT])
+    {
+        alphaS = 2;
+        alpha_x -= 1;
+        m_currentFrame = ((SDL_GetTicks() / 100) % 4);
+
+    }
+
+    else if (currentKeyStates[SDL_SCANCODE_RIGHT])
+    {
+        alphaS = 1;
+        alpha_x += 1;
+        m_currentFrame = ((SDL_GetTicks() / 100) % 4);
+
+    }
+
+    else if (currentKeyStates[SDL_SCANCODE_UP])
+    {
+        alphaS = 3;
+        alpha_y -= 1;
+        m_currentFrame = ((SDL_GetTicks() / 100) % 4);
+    }
+
+    else if (currentKeyStates[SDL_SCANCODE_DOWN])
+    {
+        alphaS = 4;
+        alpha_y += 1;
+        m_currentFrame = ((SDL_GetTicks() / 100) % 4);
+    }
 }
 
 
@@ -46,12 +84,18 @@ void Game::render()
     
     SDL_RenderClear(m_pRenderer);
     
-    TheTextureManager::Instance()->draw("animate", 0, 0, 128, 82, m_pRenderer);
-    TheTextureManager::Instance()->drawFrame("animate", 100, 100, 128, 82, 0, m_currentFrame, m_pRenderer);
+    if (alphaS == 1)
+        TheTextureManager::Instance()->drawFrame("Chr", alpha_x, alpha_y, 50, 50, 0, m_currentFrame, m_pRenderer);
+    else if (alphaS == 2)
+        TheTextureManager::Instance()->drawFrame("Chr", alpha_x, alpha_y, 50, 50, 1, m_currentFrame, m_pRenderer);
+    else if (alphaS == 3)
+        TheTextureManager::Instance()->drawFrame("Chr", alpha_x, alpha_y, 50, 50, 2, m_currentFrame, m_pRenderer);
+    else if (alphaS == 4)
+        TheTextureManager::Instance()->drawFrame("Chr", alpha_x, alpha_y, 50, 50, 3, m_currentFrame, m_pRenderer);
 
   
     SDL_RenderPresent(m_pRenderer);
-    
+    TheTextureManager::Instance()->drawFrame("Chr", alpha_x, alpha_y, 50, 50, 0, m_currentFrame, m_pRenderer);
 }
 
 bool Game::running()
@@ -61,17 +105,16 @@ bool Game::running()
 
 void Game::handleEvents()
 {
-    SDL_Event e;
-    while (SDL_PollEvent(&e))
+    SDL_Event event;
+    if (SDL_PollEvent(&event))
     {
-        if (e.type == SDL_QUIT)
-            m_bRunning = false;
-        else if (e.type == SDL_KEYDOWN)
+        switch (event.type)
         {
-            switch (e.key.keysym.sym) {
-            case SDLK_LEFT:
-                x--;
-            }
+        case SDL_QUIT:
+            m_bRunning = false;
+            break;
+        default:
+            break;
         }
     }
 }
--- src/Game.h
+++ src/Game.h
@@ -3,6 +3,19 @@
 #include <SDL2/SDL.h>
 #include "TextureManager.h"
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+const int LEVEL_WIDTH = 640;
+const int LEVEL_HEIGHT = 480;
+
+const int TILE_WIDTH = 80;
+const int TILE_HEIGHT = 80;
+const int TOTAL_TILES = 192;
+const int TOTAL_TILE_SPRITES = 12;
+
+const int TILE_RED = 0;
+const int TILE_GREEN = 1;
 
 class Game
 {
@@ -16,7 +29,13 @@ public:
 	bool running();
 	void handleEvents();
 	void clean();
-
+	void man();
+	
+	int alphaS = 1;
+	int alpha_x;
+	int alpha_y;
+	const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+	
 private:
 	SDL_Window* m_pWindow;
 	SDL_Renderer* m_pRenderer;
